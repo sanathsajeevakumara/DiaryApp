3401e7183edac4e3dc56779b3d2f873238affe1d
Sanath Sajeeva <sanath.sajeeva@pickme.lk>
2023-10-08 22:39:35 +0530

    Implement One-Tap Sign in with Google

--- app/src/main/java/com/sanathcoding/diaryapp/navigation/AuthenticationScreenRoute.kt
+++ app/src/main/java/com/sanathcoding/diaryapp/navigation/AuthenticationScreenRoute.kt
@@ -3,12 +3,22 @@ package com.sanathcoding.diaryapp.navigation
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.sanathcoding.diaryapp.presentation.screen.auth.AuthenticationScreen
+import com.stevdzasan.messagebar.rememberMessageBarState
+import com.stevdzasan.onetap.rememberOneTapSignInState
 
 fun NavGraphBuilder.authenticationScreenRoute() {
     composable(route = Screen.Authentication.route) {
+
+        val oneTapState = rememberOneTapSignInState()
+        val messageBarState = rememberMessageBarState()
+
         AuthenticationScreen(
-            loadingState = false,
-            onButtonClicked = {}
+            loadingState = oneTapState.opened,
+            oneTapState = oneTapState,
+            messageBarState = messageBarState,
+            onButtonClicked = {
+                oneTapState.open()
+            }
         )
     }
 }
--- app/src/main/java/com/sanathcoding/diaryapp/presentation/screen/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/sanathcoding/diaryapp/presentation/screen/auth/AuthenticationScreen.kt
@@ -1,23 +1,47 @@
 package com.sanathcoding.diaryapp.presentation.screen.auth
 
 import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import com.sanathcoding.diaryapp.util.Constant.CLIENT_ID
+import com.stevdzasan.messagebar.ContentWithMessageBar
+import com.stevdzasan.messagebar.MessageBarState
+import com.stevdzasan.onetap.OneTapSignInState
+import com.stevdzasan.onetap.OneTapSignInWithGoogle
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AuthenticationScreen(
     loadingState: Boolean,
-    onButtonClicked: () -> Unit
+    oneTapState: OneTapSignInState,
+    messageBarState: MessageBarState,
+    onButtonClicked: () -> Unit,
 ) {
     Scaffold(
         content = {
-            AuthenticationContent(
-                loadingState = loadingState,
-                onButtonClicked = onButtonClicked
-            )
+            ContentWithMessageBar(messageBarState = messageBarState) {
+                AuthenticationContent(
+                    loadingState = loadingState,
+                    onButtonClicked = onButtonClicked
+                )
+            }
+        }
+    )
+
+    OneTapSignInWithGoogle(
+        state = oneTapState,
+        clientId = CLIENT_ID,
+        onTokenIdReceived = { token ->
+            Log.d("Auth", "Token : $token")
+            messageBarState.addSuccess("Successfully Authenticated!")
+        },
+        onDialogDismissed = { message ->
+            Log.d("Auth", "Message : $message")
+            messageBarState.addError(Exception(message))
         }
     )
 }
