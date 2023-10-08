52d13adba2cf833d5ee9306806d01c3f58ff56b4
Sanath Sajeeva <sanath.sajeeva@pickme.lk>
2023-10-08 22:19:11 +0530

    Design Authentication Screen

--- app/src/main/java/com/sanathcoding/diaryapp/navigation/AuthenticationScreenRoute.kt
+++ app/src/main/java/com/sanathcoding/diaryapp/navigation/AuthenticationScreenRoute.kt
@@ -2,9 +2,13 @@ package com.sanathcoding.diaryapp.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.sanathcoding.diaryapp.presentation.screen.auth.AuthenticationScreen
 
 fun NavGraphBuilder.authenticationScreenRoute() {
     composable(route = Screen.Authentication.route) {
-        
+        AuthenticationScreen(
+            loadingState = false,
+            onButtonClicked = {}
+        )
     }
 }
