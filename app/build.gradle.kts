f90103c5056abfe0eb9438d51765ef3329b32a07
Sanath Sajeeva <sanath.sajeeva@pickme.lk>
2023-10-17 05:12:09 +0530

    Create a DateHeader as a Sticky Header

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
     kotlinOptions {
         jvmTarget = "17"
